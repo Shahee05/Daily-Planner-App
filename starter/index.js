c4074a13282136c4bc09996c4a68fc3179667986
Shahee05 <shaherun_islam@hotmail.co.uk>
2023-03-24 03:26:34 +0000

    fixed adding rows and save buttons with help

--- starter/index.js
+++ starter/index.js
@@ -4,17 +4,107 @@ $(document).ready(function () {
     let currentDay = moment().format("dddd Do MMMM, YYYY");
     $("#currentDay").text(currentDay);
 
-    // Define an array to store the time values for each hour
+// Forming a working schedule between 9 -5pm
     let hours = [];
+    for (let i = 9; i <= 17; i++) {
+        let hour = (i < 12 ? i + "am" : (i === 12 ? i + "pm" : (i - 12) + "pm"));
+        hours.push(hour);
+    }
 
-    // // Loop through the hours from 9am to 5pm and format them
-    // for (let i = 9; i <= 17; i++) {
-    //     let hour = (i < 12 ? i + "am" : (i === 12 ? i + "pm" : (i - 12) + "pm"));
-    //     hours.push(hour);
-    // }
 
-    //   // build calendar by row for fix set of hours
-    //   for (let hour = 9; hour <= 17; hour++) {
-    //     // index for array use offset from hour
-    //     let index = hour - 9;
+
+    // creating time blocks
+    hours.forEach((hour, index) => {
+        let timeBlockSlot = $("<div>").addClass("time-block row");
+        let hourSlot = $("<div>").addClass("hour col-2").text(hour);
+        // ID for each input
+        let scheduleInput = $("<input>")
+            .addClass("scheduleInput col-8")
+            .attr("id", `input-${index}`
+            );
+
+
+
+        // Local Storage
+        let currentNewEvent = `inputActivity${index}`;
+        let storedSchedule = localStorage.getItem(currentNewEvent);
+
+        //If there is a stored schedule, we want to save it if its form today hence use split to divide the array
+        if (storedSchedule) {
+            let splitInputActivity = storedSchedule.split("<>");
+            if (splitInputActivity[1] === currentDay) {
+                scheduleInput.val(splitInputActivity[0]);
+            } else {
+                // if not from today, remove from storage
+                localStorage.removeItem(currentNewEvent);
+            }
+        }
+
+        // make a save button
+        let saveData = $("<button>")
+            .addClass("saveBtn col-2")
+            .html("<i class='fas fa-save'></i>");
+
+        // click save button
+        saveData.click(function () {
+            // Save the current schedule to local storage with the date
+            localStorage.setItem(
+                currentNewEvent,
+                scheduleInput.val() + "<>" + currentDay
+            );
+
+            // saveCHanges
+            let timeout;
+            let saveMessage = $("<p>").addClass("message").text("New changes have been made!");
+            if (timeout != null) {
+                clearTimeout(timeout);
+            } else {
+                $(".jumbotron").append(saveMessage);
+            }
+
+        });
+
+        timeBlockSlot.append(hourSlot).append(scheduleInput).append(saveData);
+
+        $(".container").append(timeBlockSlot);
+
+    });
+
+    // check if the hour is in the past present or future and assign a class
+    for (let i = 0; i < hours.length; i++) {
+
+        // starts at 0 so +9 (0 + 9) so that it represents 9am
+        let eachtimeBlock = moment().hour(i + 9);
+        // input-${i} corresponds with the unique id of each element 
+        let currentIndex = $(`#input-${i}`);
+
+        if (moment().isBefore(eachtimeBlock)) {
+            currentIndex.addClass("future");
+        } else if (moment().isAfter(eachtimeBlock)) {
+            currentIndex.addClass("past");
+        } else {
+            currentIndex.addClass("present");
+        }
+    }
 });
+
+//attempted colour change grid to differentiate between past present and future 
+
+function colorChange() {
+    var hourBlock = moment().hours();
+  
+    $(".time-block").each(function () {
+      var currentHour = parseInt($(this).attr("id"));
+  
+      if (currentHour > hourBlock) {
+        $(this).addClass("future");
+      } else if (currentHour === hourBlock) {
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("past");
+      }
+    });
+  }
+  
+  colorChange();
+
